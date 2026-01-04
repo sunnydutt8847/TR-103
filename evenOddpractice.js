f5196f1eec7b1dfb33766866639b1ed714a9ada6
sunnydutt8847 <sunnydutt884729@gmail.com>
2026-01-04 16:46:42 +0530

    Create evenOddpractice.js

---
+++ evenOddpractice.js
@@ -0,0 +1,17 @@
+// practicing loop's logic where if i want to print 1 - 50 how many even number are.
+for (let i = 0; i <= 50; i++) {
+    if (i % 2 == 0) {
+        console.log(i);
+
+
+    }
+}
+
+
+for (let i = 0; i <= 50; i++) {
+    if (i % 2 == 0 && i%3==0) {
+        console.log(i);
+
+
+    }
+}
