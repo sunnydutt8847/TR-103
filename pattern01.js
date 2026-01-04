ef50269d2e7e2cad1eac6d24776d26ab8b19ce18
sunnydutt8847 <sunnydutt884729@gmail.com>
2026-01-04 16:41:08 +0530

    Create pattern01.js

---
+++ pattern01.js
@@ -0,0 +1,14 @@
+// this pattern using for loop
+// + 
+// + + 
+// + + + 
+
+for(let i =1; i<=3; i++){
+    let str= ".."
+    for(let j=1; j<=i; j++){
+        str += "+ ";
+
+        
+    }
+    console.log(str)
+}
